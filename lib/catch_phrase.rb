9afabfb48c2d7d1ad599ccd89b341190dafcaef8
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-06-28 16:37:53 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 def catch_phrase(phrase = "It's-a me, Mario!")
+=======
+phrase = "It's-a me, Mario!"
+def catch_phrase(phrase)
+>>>>>>> e665cd3fb6d26f943158d7d9a5f16f7a3fbb01d1
   puts phrase
 end
